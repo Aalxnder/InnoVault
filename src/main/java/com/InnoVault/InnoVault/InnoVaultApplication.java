953ac2d3dc6b71c97e3a2b778acd85ba00f8da3e
alx07siu <navarrita20@gmail.com>
2024-03-19 19:23:40 -0600

    Initial commit

---
+++ src/main/java/com/InnoVault/InnoVault/InnoVaultApplication.java
@@ -0,0 +1,13 @@
+package com.InnoVault.InnoVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InnoVault/InnoVault/InnoVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InnoVault.InnoVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnoVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
